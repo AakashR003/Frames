abc4f772398be6429c415be00c6625cde891e059
AakashR003 <aakashravichandran03@gmail.com>
2025-07-18 23:13:35 +0530

    EigenValue Implemented in 1st order Analysis

--- FirstOrderResponse.py
+++ FirstOrderResponse.py
@@ -1,15 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.linalg import eig
+import math
 
 
 try:
     from .Model import Model
+    from .Loads import NeumanBC
     from .StructuralElements import Node, Member
     from .config import config
     from .Computer import Computer
     from .Functions import max_nested
 except:
     from Model import Model
+    from Loads import NeumanBC
     from StructuralElements import Node, Member
     from config import config
     from Computer import Computer
@@ -53,6 +57,56 @@ class FirstOrderGlobalResponse(Model):
         #force dict formation
         return SupportForces
     
+    def EigenAnalysis(self, Solver = "eigs"):
+        StiffnessMatrix = self.GlobalStiffnessMatrixCondensed()
+        CriticalLoad , EigenMode = eig(StiffnessMatrix)
+        EigenMode = Computer.EigenSolver(StiffnessMatrix, Solver = Solver, k=len(self.UnConstrainedDoF()))
+
+        return min(filter(math.isfinite, [abs(z.real) for z in CriticalLoad])),CriticalLoad, EigenMode
+
+    def MemberEigenMode(self, MemberNumber, scale_factor = 1, EigenModeNo = 1, EigenVectorDict = None):
+        
+        FEDivision = config.get_FEDivision()
+        MemberNo = int(MemberNumber)
+        member = self.Members[MemberNo - 1]
+        length = member.length()
+
+        if EigenVectorDict == None:
+            EigenVector = self.EigenAnalysis(EigenModeNo = EigenModeNo)[2][:, (EigenModeNo-1)]
+            EigenVectorDict = Computer.ModelDisplacementList_To_Dict(EigenVector, self.UnConstrainedDoF, self.TotalDoF)
+
+        EigenVectorGlobal = Computer.ModelDisplacement_To_MemberDisplacement(MemberNo, EigenVectorDict, self.Members)
+        EigenVectorLocal = np.dot((self.Members[MemberNo-1].Transformation_Matrix()), EigenVectorGlobal)
+        self.DeflectionPosition, BeamEigenVector = Computer.Linear_Interpolate_Displacements(EigenVectorLocal, length, FEDivision, scale_factor)
+        
+        return BeamEigenVector
+    
+    def CalculateModalStiffness(self):
+
+        SM = self.GlobalStiffnessMatrixCondensed()
+        OrthogonalStiffnessMatrix = Computer.OrthogonalSolver(SM, SMatrix= SM)
+        row_max_abs = np.max(np.abs(OrthogonalStiffnessMatrix), axis=1)
+
+        return row_max_abs
+    
+    def OrthogonalForceVector(self):
+        """
+        Returns the critical force points for the model for each eigenmode
+        """
+        #todo: Circular Import is not checked - NeumanBC
+
+        Force_Vector = np.array(self.ForceVector())
+        OrthogonalForceVector = Computer.OrthogonalSolver(Force_Vector, SMatrix=self.GlobalStiffnessMatrixCondensed())
+
+        abs_with_index = [(i, abs(val)) for i, val in enumerate(OrthogonalForceVector)]
+        # Sort by abs value descending
+        abs_with_index.sort(key=lambda x: x[1], reverse=True)
+
+        print("Contribution, Mode",abs_with_index)
+
+        return OrthogonalForceVector
+
+    
     def LoadDisplacementTrace(self, NodeNumber = 1, Direction = "x", LoadFactor = None, division =20):
         """
         Calculates the load-displacement trace for a specific DOF.
@@ -87,6 +141,61 @@ class FirstOrderGlobalResponse(Model):
                 self.Loads[i].Magnitude = self.Loads[i].Magnitude / load
         
         return LoadTrace, DisplacementTrace
+    
+    def PlotEigenMode(self, EigenModeNo = 1, scale_factor = 1, Solver ="eigsh", show_structure = True):
+        fig, ax = plt.subplots(figsize=(12, 8))
+
+        
+        if show_structure:
+            computer_instance = Computer()
+            computer_instance.PlotStructuralElements(ax,self.Members, self.Points, ShowNodeNumber = False)
+
+        Eigen = self.EigenAnalysis(Solver = Solver)
+        print("Eigen Load", Eigen[0], Eigen[1])
+        EigenVector = Eigen[2][:,(EigenModeNo-1)]
+        EigenVectorDict = Computer.ModelDisplacementList_To_Dict(EigenVector, self.UnConstrainedDoF, self.TotalDoF)
+        
+        # Determine global maximum absolute EigenDisp for scaling
+        max_abs_Eigendeflection = max_nested(EigenVector)
+        scale_factor = scale_factor / max_abs_Eigendeflection
+        
+        # Plot BMD for each member as lines
+        for member_idx, member in enumerate(self.Members):
+            # Get member properties
+            start = member.Start_Node
+            end = member.End_Node
+            L = member.length()
+            
+            # Get values and positions
+            EigenModeDeflections = self.MemberEigenMode(member_idx+1, scale_factor, EigenVectorDict = EigenVectorDict)
+            positions = self.DeflectionPosition
+            
+            # Calculate member orientation
+            dx = end.xcoordinate - start.xcoordinate
+            dy = end.ycoordinate - start.ycoordinate
+            angle = np.arctan2(dy, dx)
+            
+            # Create perpendicular direction vector
+            perp_dir = np.array([-np.sin(angle), np.cos(angle)])
+            
+            # Create points for BMD visualization
+            x_points = []
+            y_points = []
+            for pos, deflection in zip(positions, EigenModeDeflections):
+                # Calculate position along member
+                x_pos = start.xcoordinate + (dx * pos/L)
+                y_pos = start.ycoordinate + (dy * pos/L)
+                
+                # Offset by value in perpendicular direction
+                x_points.append(x_pos + perp_dir[0] * deflection)
+                y_points.append(y_pos + perp_dir[1] * deflection)
+            
+            # Plot as simple black line
+            ax.plot(x_points, y_points, color='red', linewidth = 2)
+
+        ax.set_title(f"Linear Eigen Mode {EigenModeNo} - {Eigen[1][EigenModeNo-1]}")
+        ax.axis('equal')
+        plt.show()
 
    
     def PlotLoadDisplacementCurve(self, NodeNumber = 1, Direction = "x", LoadFactor = None, division = 20):
